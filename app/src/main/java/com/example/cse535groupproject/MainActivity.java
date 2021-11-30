2e32b5ac89f65b19aba3ea573429bd98506fe761
zhi hu <52951338+DrakDay@users.noreply.github.com>
2021-11-30 14:01:18 -0700

    master battery done

--- app/src/main/java/com/example/cse535groupproject/MainActivity.java
+++ app/src/main/java/com/example/cse535groupproject/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.cse535groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver batterylevelReciver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            Log.i("TAG", "onReceive: " + Integer.toString(level));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.registerReceiver(this.batterylevelReciver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
+
+
 }
