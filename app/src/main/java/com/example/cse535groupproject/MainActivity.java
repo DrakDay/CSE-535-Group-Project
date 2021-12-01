f28ce75d0c2f776fe5bad09e0a245453e0860524
zhi hu <52951338+DrakDay@users.noreply.github.com>
2021-11-30 17:21:50 -0700

    client-server communication established

--- app/src/main/java/com/example/cse535groupproject/MainActivity.java
+++ app/src/main/java/com/example/cse535groupproject/MainActivity.java
@@ -6,27 +6,192 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int battery_level;
+
+    // battery level check
     private BroadcastReceiver batterylevelReciver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            Log.i("TAG", "onReceive: " + Integer.toString(level));
+            battery_level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            Log.i("TAG", "battery level: " + Integer.toString(battery_level));
         }
     };
+
+    //setting up a server
+    private ServerSocket serverSocket;
+    private Socket tempClientSocket;
+    Thread serverThread = null;
+    public static String SERVER_IP = "";
+    public static final int SERVER_PORT = 8888;
+
+
+    //ui element
+    TextView server_ip_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        server_ip_address = findViewById(R.id.server_ip_address);
 
+        //listening batter level check
         this.registerReceiver(this.batterylevelReciver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+
+        //set up server ip
+        try{
+            SERVER_IP = getLocalIPaddress();
+            server_ip_address.setText("master server ip address : " + SERVER_IP);
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+
+        //starting server
+        this.serverThread = new Thread(new ServerThread());
+        this.serverThread.start();
+
+        //testing sending msg to client
+        /*
+        int i = 0;
+        while(true){
+            sendMessage(Integer.toString(i));
+            Log.i("TAG", "send :" + Integer.toString(i));
+            i++;
+        }
+         */
+
     }
 
+    // get master local ip address
+    private String getLocalIPaddress() throws UnknownHostException{
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        assert wifiManager != null;
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipInt = wifiInfo.getIpAddress();
+        return InetAddress.getByAddress(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(ipInt).array()).getHostAddress();
+    }
 
+    // server thread
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+                Log.i("TAG", "Starting Server :");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("TAG", "Starting Server failed:" + e.getMessage());
+            }
+            if (null != serverSocket) {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        socket = serverSocket.accept();
+                        CommunicationThread commThread = new CommunicationThread(socket);
+                        new Thread(commThread).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.i("TAG", "Communicating to Client failed:" + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    // communication thread
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            tempClientSocket = clientSocket;
+            try {
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                Log.i("TAG", "Connecting to Client failed");
+            }
+            Log.i("TAG", "Connected to Client");
+        }
+
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String message = input.readLine();
+                    if (null == message || "Disconnect".contentEquals(message)) {
+                        Thread.interrupted();
+                        message = "Client Disconnected";
+                        Log.i("TAG", "Client : (most time is Client Disconnected )" + message);
+                        break;
+                    }
+                    Log.i("TAG", "Client:" + message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    private void sendMessage(final String message) {
+        try {
+            if (null != tempClientSocket) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PrintWriter out = null;
+                        try {
+                            out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(tempClientSocket.getOutputStream())),
+                                    true);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        out.println(message);
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != serverThread) {
+            serverThread.interrupt();
+            serverThread = null;
+        }
+    }
 }
